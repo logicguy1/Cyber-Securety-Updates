289a36bd8dfdfa353b4f73c58b3eb2c9cd16a279
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-03-09 20:56:33 +0100

    Update code

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import feedparser
+import requests
 import time
+
+webhook = input("Webhook url: ")
 
 def get_entry(lastEntry):
     NewsFeed = feedparser.parse("https://threatpost.com/feed/")
     entry = NewsFeed.entries[0]
 
-    if lastEntry != entry:
-        print(entry.title)
-        print(entry.summary)
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
+    if lastEntry.title != entry.title:
+        requests.post(webhook, data = {"content" : f"```md\n{entry.title}\n{'=' * len(entry.title)}\n{entry.description}\n\n[][ {entry.link} ][]```"})
     time.sleep(60)
 
     get_entry(entry)
 
 get_entry("")
-  
